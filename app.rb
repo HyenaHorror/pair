5de3739a5f902c8e61654352abb121b5d10ac50d
Rob Bird <robertbirdwv@gmail.com>
2018-03-21 02:26:01 -0400

    First name successfully pushed into array.

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ enable :sessions
 get '/' do
   erb :index
 end
+
+post '/add_name' do
+  session[:name_array] = Array.new
+  session[:name_array]<< params[:new_name]
+  redirect '/add_more'
+end
+
+get '/add_more' do
+  name_list = session[:name_array]
+
+  erb :add_more, locals: {name_list:name_list}
+end
