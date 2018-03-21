ca90e55652292f5e639b3d49e75684b17918dafc
Rob Bird <robertbirdwv@gmail.com>
2018-03-21 02:29:15 -0400

    Successfully pushing multiple names.

--- app.rb
+++ app.rb
@@ -2,18 +2,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 enable :sessions
 
+
 get '/' do
+  session[:name_array] = Array.new
   erb :index
 end
 
 post '/add_name' do
-  session[:name_array] = Array.new
-  session[:name_array]<< params[:new_name]
+  session[:name_array].push params[:new_name]
   redirect '/add_more'
 end
 
 get '/add_more' do
   name_list = session[:name_array]
-
   erb :add_more, locals: {name_list:name_list}
 end
