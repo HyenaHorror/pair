eaa87dd1767797670764498154543efd0a570eff
Rob Bird <robertbirdwv@gmail.com>
2018-03-30 10:14:02 -0400

    Removed 'add more' page.

--- app.rb
+++ app.rb
@@ -1,26 +1,16 @@
 require 'sinatra'
-# require 'sinatra/reloader'
+require 'sinatra/reloader'
 require_relative 'pair.rb'
 enable :sessions
 
 
 get '/' do
-  session[:name_array] = Array.new
   erb :index
 end
 
-post '/add_name' do
-  session[:name_array].push params[:new_name]
-  redirect '/add_more'
-end
-
-get '/add_more' do
-  name_list = session[:name_array]
-  erb :add_more, locals: {name_list:name_list}
-end
-
 post '/make_pairs' do
-  pair_list = pairing(*session[:name_array])
+  pair_list = Array.new
+  pair_list = pairing(*params[:new_name])
   session[:pair_list] = pair_list #Must set to session variable for next step.
   erb :display_pairs, locals: {pair_list:pair_list}
 end
